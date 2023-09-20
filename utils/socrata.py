474662443925fb63b72ec9a1866c5f4da1457359
Germán Muñoz <46906587+munozbravo@users.noreply.github.com>
2023-09-20 13:05:36 -0500

    Agrega payload_proponentes.

--- utils/socrata.py
+++ utils/socrata.py
@@ -89,3 +89,55 @@ def payload_entidades(offset=1000, selection=None, select_col=None, sort=None):
         payload.update({"$where": where_query})
 
     return payload
+
+
+def payload_proponentes(
+    fechas: tuple[date] | date,
+    offset: int = 1000,
+    id_proc: str = None,
+    proveedor: str = None,
+) -> dict:
+    """Payload para Proponentes por Proceso SECOP II
+
+    Parameters
+    ----------
+    fechas : tuple[date] | date
+        Fechas inicial y final de búsqueda
+    offset : int, optional
+        Cantidad de resultados por llamado, default 1000
+    id_proc : str, optional
+        ID del proceso de compra, default None
+    proveedor : str, optional
+        Nombre del proveedor a buscar, default None
+
+    Returns
+    -------
+    dict
+        Payload para enviar a Socrata API
+    """
+    # https://dev.socrata.com/foundry/www.datos.gov.co/hgi6-6wh3
+
+    payload = {"$limit": offset, "$order": "fecha_publicaci_n DESC"}
+
+    if isinstance(fechas, tuple):
+        inicio, fin = fechas
+    else:
+        inicio = fechas
+        fin = date.today()
+
+    inicial = f'{inicio.strftime("%Y-%m-%d")}T00:00:00'
+    final = f'{fin.strftime("%Y-%m-%d")}T23:59:59'
+
+    where_query = f"fecha_publicaci_n between '{inicial}' and '{final}'"
+
+    if id_proc is not None:
+        where_query = f"{where_query} AND id_procedimiento = '{id_proc}'"
+
+    if proveedor is not None:
+        provee = "%".join(proveedor.split())
+        where_query = f"{where_query} AND upper(proveedor) like upper('%{provee}%')"
+
+    if where_query:
+        payload.update({"$where": where_query})
+
+    return payload
