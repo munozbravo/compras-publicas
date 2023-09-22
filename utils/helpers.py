2134efbcf181a1b1614788774fdb4e5a35232738
Germán Muñoz <46906587+munozbravo@users.noreply.github.com>
2023-09-22 12:23:22 -0500

    Agrega fn para validar fechas.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 import pandas as pd
 
 
@@ -24,3 +26,35 @@ def normalizar_textual(df: pd.DataFrame, col: str) -> pd.Series:
     )
 
     return normalizada
+
+
+def validar_fechas(fechas: tuple | date = None) -> tuple[date]:
+    """Comprueba retorno de fechas inicial y final
+
+    Parameters
+    ----------
+    fechas : tuple | date, optional
+        Fechas inicial y final, default None
+
+    Returns
+    -------
+    tuple
+        Fechas inicial y final
+    """
+    if isinstance(fechas, tuple):
+        if len(fechas) == 2:
+            inicio, fin = fechas
+        elif len(fechas) == 1:
+            inicio = fechas[0]
+            fin = date.today()
+        else:
+            fin = date.today()
+            inicio = fin - timedelta(days=1)
+    elif isinstance(fechas, date):
+        inicio = fechas
+        fin = date.today()
+    else:
+        fin = date.today()
+        inicio = fin - timedelta(days=1)
+
+    return (inicio, fin)
